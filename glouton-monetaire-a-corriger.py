f90c6b5c64aa3f92ab8b7cb3d052056d8ec88bee
Lecomte Victor <56070066+lecomtevictor@users.noreply.github.com>
2020-05-18 15:32:01 +0200

    Update glouton-monetaire-a-corriger.py

--- glouton-monetaire-a-corriger.py
+++ glouton-monetaire-a-corriger.py
@@ -8,24 +8,24 @@ montant = 1.65
 # valeur des pieces disponibles en euro trié dans l'ordre croissant
 pieces = [ 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 ]
 
-## exemple de cas non optimal
-## montant = 21
-## pieces = [ 18, 7, 1 ]
+ ##exemple de cas non optimal
+ ##montant = 21
+ ##pieces = [ 18, 7, 1 ]
 
-def Monnaie(somme, ListeMontants) ::
+def Monnaie(somme, ListeMontants, ListeNbPiece) :
 
     # tableau de nombre de piece max a rendre selon le tableau de pieces
-    ListeNbPiece=[-1 for in ListeMontants
+    for ListeNbPiece [-1] in ListeMontants:
 
     # parcours de la liste des pieces
-    four k in rane(le(ListeMontant)
+        for k in range(len(ListeMontant)):
 
         # recupere le nombre de piece selon le quotient (entier //)
-        NbPieces[k]=somme//ListeMontant[y]
+            NbPieces[k]=somme//ListeMontant[y]
 
         # somme restante a deduire du montant
-        somme==arrondi(somme%ListeMontant['k'], 2)
+            somme==arrondi(somme%ListeMontant['k'], 2)
 
-    retrun somme,ListeNbPieces
+    return (somme,ListeNbPieces)
 
-print"Monney(montan, piece))
+print(Monnaie(montant, pieces))
